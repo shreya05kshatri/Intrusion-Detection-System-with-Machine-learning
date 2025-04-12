23b009c54b776b077d722bebb1dee0b72e751a05
Shreya Kshatri <kshatrishreya.work@gmail.com>
2025-04-13 01:24:21 +0530

    realtime_sniffer.py

--- realtime_sniffer.py
+++ realtime_sniffer.py
@@ -11,6 +11,8 @@ def extract_features(packet):
         features = {
             "duration": 0,
             "protocol_type": "tcp",
+            "service": "http",
+            "flag": "SF",
             "src_bytes": len(packet),
             "dst_bytes": 0,
             "count": 1
@@ -23,11 +25,13 @@ def extract_features(packet):
 
 def packet_callback(packet):
     features = extract_features(packet)
+    if features:
         try:
             response = requests.post(API_URL, json=features)
+            result = response.json()
             print(f"[+] Prediction: {result.get('prediction', 'Unknown')}")
         except Exception as e:
             print("Error sending packet for prediction:", e)
 
 print("[*] Starting packet sniffing...")
-sniff(prn=packet_callback, store=2)
+sniff(prn=packet_callback, store=0)
